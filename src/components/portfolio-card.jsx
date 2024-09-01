0eb5496fad38ce1d41409a9c1b3bb1639d130738
Raul Maulidhino <raulahmadm314@gmail.com>
2024-09-01 20:28:08 +0700

    edit image preview's formats

--- src/components/portfolio-card.jsx
+++ src/components/portfolio-card.jsx
@@ -1,8 +1,11 @@
-export default function PortfolioCard({preview, title, link}) {
+export default function PortfolioCard({preview, fallback_preview, title, link}) {
     return (
         <section className="flex flex-col">
             <a href={ link } target="_blank">
-                <img src={ preview } alt={ title } className="w-full h-full rounded-t-lg object-cover" />
+                <picture>
+                    <source srcset={ preview } type="image/webp" />
+                    <img src= { fallback_preview } alt={ title } className="w-full h-full rounded-t-lg object-cover" />
+                </picture>
             </a>
             <section className="bg-opacity-50 bg-neon-magenta text-center text-md rounded-b-lg font-semibold py-2 px-4">
                 { title }
