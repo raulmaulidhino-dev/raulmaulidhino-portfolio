f4b4094dcf52998d150b222a3bbdbf2bbd223fef
Raul Maulidhino <raulahmadm314@gmail.com>
2024-10-05 13:32:08 +0700

    fix: resolve animation troubles that cause overflow

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -6,7 +6,7 @@ import { TbBrandGithub as Github } from "react-icons/tb"
 export default function ContactMe() {
     return (
         <footer id="contact_me" className="px-10 py-[6rem]">
-            <h2 className="text-neon-cyan font-extrabold text-center text-5xl font-special md:text-6xl mb-10" data-aos="zoom-out">CATCH ME!</h2>
+            <h2 className="text-neon-cyan font-extrabold text-center text-5xl font-special md:text-6xl mb-10" data-aos="zoom-in">CATCH ME!</h2>
             <h3 className="text-xl md:text-3xl text-center font-semibold mb-[1em]" data-aos="fade-down"><span className="font-extrabold"><strong>Feel Free</strong></span> to reach me out!</h3>
             <section className="flex gap-4 justify-center items-center">
                 <a href="mailto:raul.maulidhino@gmail.com" target="_blank" data-aos="fade-up" data-aos-duration="1500" data-aos-easing="ease-in-out-sine"><Mail size="42" /></a>
