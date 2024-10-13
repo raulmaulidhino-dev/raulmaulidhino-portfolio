d3b4fcf53071a07365bfd3b1f68448fa56d7c400
Raul Maulidhino <raulahmadm314@gmail.com>
2024-10-13 13:22:21 +0700

    style: change a property name to a clear and descriptive name

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,10 +1,10 @@
-export default function PortfolioCard({preview, fallback_preview, title, link}) {
+export default function PortfolioCard({preview, preview_fallback, title, link}) {
     return (
         <section className="flex flex-col" data-aos="fade-down">
             <a href={ link } target="_blank">
                 <picture>
                     <source srcSet={ preview } type="image/webp" />
-                    <img src= { fallback_preview } alt={ title } className="w-full h-full rounded-t-lg object-cover" />
+                    <img src= { preview_fallback } alt={ title } className="w-full h-full rounded-t-lg object-cover" />
                 </picture>
             </a>
             <section className="bg-opacity-50 bg-neon-magenta text-center text-md rounded-b-lg font-semibold py-2 px-4">
