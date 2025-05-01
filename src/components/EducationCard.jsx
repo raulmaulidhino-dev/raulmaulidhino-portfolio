a00eba69d2025d1213a0133f8c23c3a2b8440893
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-01 15:22:41 +0700

    style: enhance the layout

--- src/components/EducationCard.jsx
+++ src/components/EducationCard.jsx
@@ -1,8 +1,12 @@
-export default function EducationCard({schoolName, startYear, endYear, description}) {
+export default function EducationCard({schoolName, startYear, endYear, description, schoolLogo, schoolLogo_fallback}) {
     return(
-        <section className="card flex flex-col items-start" data-aos="fade-down">
-            <div className="bg-neon-blue even:bg-neon-violet text-dark-blue text-sm font-semibold px-2 py-1 my-4 rounded-md inline-block">{ startYear } - { endYear }</div>
-            <h3 className="text-lg font-bold">{ schoolName }</h3>
+        <section className="card text-white bg-dark-blue border-neon-blue even:border-neon-magenta border-2 p-6 rounded-lg flex flex-col items-start" data-aos="fade-down">
+            <picture className="w-3/4 mx-auto">
+                <source srcSet={ schoolLogo } type="image/webp" />
+                <img src= { schoolLogo_fallback } alt={ schoolName } className="w-full h-full p-4 md:p-10 object-cover transition-all duration-700 ease-in-out hover:scale-[1.25]" />
+            </picture>
+            <div className="bg-neon-blue text-dark-blue text-sm font-semibold px-2 py-1 my-4 rounded-md inline-block">{ startYear } - { endYear }</div>
+            <h3 className="text-lg font-extrabold mb-[1em]">{ schoolName }</h3>
             <p>{ description }</p>
         </section>
     )
