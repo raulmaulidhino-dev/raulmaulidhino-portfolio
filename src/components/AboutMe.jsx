65b88d00559c86edd682b2002a8436a6089ae3bc
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-01 15:21:46 +0700

    feat: add dynamic age

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,9 +4,11 @@ import DeveloperPicture from '../assets/developer-picture.webp';
 import DeveloperPicture_fallback from '../assets/developer-picture.jpg';
 
 
-import { TbDragDrop as Design, TbCode as Code, TbTex as Prompt} from 'react-icons/tb';
+import { TbDragDrop as Design, TbCode as Code, TbTerminal2 as Prompt} from 'react-icons/tb';
 
 export default function AboutMe() {
+
+    const DOB = import.meta.env.VITE_DOB ?? "2007-01-01";
 
     const myDo = [
         {icon: <Design size="72" className="text-neon-magenta" />, title: "DESIGN", description: "I design clean and user-friendly web layouts that are easy to navigate"},
@@ -24,9 +26,9 @@ export default function AboutMe() {
                         <img src= { DeveloperPicture_fallback } alt="Picture of the Developer" className="w-full h-full rounded-t-lg object-cover" />
                     </picture>
                     <div className="max-w-[500px]">
-                        <h2 className="text-xl md:text-3xl font-semibold mb-[1em]"  data-aos="fade-right">I&apos;m <span className="font-extrabold">Raul Ahmad Maulidhino</span>, Junior FE Developer / AI Enthusiast</h2>
+                        <h2 className="text-xl md:text-3xl font-semibold mb-[1em]"  data-aos="fade-right">I&apos;m <span className="font-extrabold">Raul Ahmad Maulidhino</span>, Junior Full-Stack Developer / Robotic Enthusiast</h2>
                         <p data-aos="fade-down">
-                            I&apos;m a <span>17</span>-year old student passionate about <strong className="text-neon-cyan">Coding, AI, Robotics, and Web Development</strong>.
+                            I&apos;m a <span>{ new Date(new Date() - new Date(DOB)).getUTCFullYear() - 1970 }</span>-year old student passionate about <strong className="text-neon-cyan">Coding, AI, Robotics, and Web Development</strong>.
                             I&apos;m always exploring new technologies, dedicating my time to learn and experiment with <strong className="text-neon-magenta">Machine Learning, Automation, and Web Technologies</strong>.
                             Continuously, updating my skills, I stay on top of industry trends through self-study and hands-on projects.
                             I&apos;m excited to apply my knowledge in real-world scenarios and keep growing within these fields.
