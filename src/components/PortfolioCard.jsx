a8ec18f4dce90d7317a962562cd4500ba9a34aff
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-16 09:27:30 +0700

    style: add hover effect for buttons and labels to enhance styling

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -15,15 +15,15 @@ export default function PortfolioCard({preview, preview_fallback, title, link, d
                 <p className="text-sm  mb-[1em]">{ description }</p>
                 <div className="flex flex-wrap gap-1 mb-6">
                     {labels.map((label) => (
-                        <div className="text-xs font-bold text-neon-cyan even:text-neon-magenta bg-dark-blue py-1 px-2 border-2 border-neon-cyan even:border-neon-magenta rounded-md">{ label }</div>
+                        <div className="text-xs font-bold text-neon-cyan even:text-neon-magenta hover:text-dark-blue bg-dark-blue py-1 px-2 border-2 border-neon-cyan hover:bg-neon-cyan even:border-neon-magenta even:hover:bg-neon-magenta rounded-md">{ label }</div>
                     ))}
                 </div>
                 <div className="flex flex-wrap gap-2 mt-auto">
-                    <a href={ link } className="text-xs text-black font-bold bg-white py-1 px-3 rounded-md border-2 border-white flex gap-1 items-center">
+                    <a href={ link } className="text-xs text-black hover:text-white font-bold bg-white hover:bg-black py-1 px-3 rounded-md border-2 border-white flex gap-1 items-center">
                         <WebsiteIcon size="15" />
                         <span>Website</span>
                     </a>
-                    <a href={ sourceLink } className="text-xs text-white font-bold bg-black py-1 px-3 rounded-md border-2 border-white  flex gap-1 items-center">
+                    <a href={ sourceLink } className="text-xs text-white hover:text-black font-bold bg-black hover:bg-white py-1 px-3 rounded-md border-2 border-white flex gap-1 items-center">
                         <SourceCodeIcon size="15" />
                         <span>Source Code</span>
                     </a>
