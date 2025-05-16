15cc01ee7789fd9f77ff2ac86f5f2954bed7cabe
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-16 08:54:42 +0700

    style: enhance layout styling

--- src/components/SkillsAndStrengths.jsx
+++ src/components/SkillsAndStrengths.jsx
@@ -28,15 +28,15 @@ export default function SkillsAndStrengths() {
 
       <section>
         <h3
-          className="text-2xl md:text-3xl font-bold mb-[1em]"
+          className="text-2xl md:text-3xl text-center font-bold mb-[1em]"
           data-aos="zoom-in"
         >
-          Technical Proficiency :
+          Technical Proficiency
         </h3>
 
-        <section className="w-fit mx-auto p-6 flex flex-wrap gap-8">
-          {techSkills.map((skill, index) => (
-            <section data-aos="zoom-in" data-aos-delay={100 * (index + 1)} key={skill.title} className="max-w-40 border-b-2 border-b-neon-magenta border-l-2 border-l-neon-magenta border-r-2 border-r-neon-cyan border-t-2 border-t-neon-cyan p-6 rounded-[17px] space-y-6 hover:shadow-lg hover:shadow-neon-magenta">
+        <section className="w-full p-2 flex flex-wrap justify-center items-center gap-8">
+          {techSkills.map((skill) => (
+            <section data-aos="zoom-in" key={skill.title} className="max-w-40 bg-dark-blue border-b-2 border-b-neon-magenta border-l-2 border-l-neon-magenta border-r-2 border-r-neon-cyan border-t-2 border-t-neon-cyan p-6 rounded-[17px] space-y-6 hover:shadow-lg hover:shadow-neon-magenta">
                 <img
                     className="w-full"
                     title={skill.title}
