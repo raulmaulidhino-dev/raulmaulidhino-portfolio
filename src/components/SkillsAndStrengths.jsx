624cdfb836c27826d53caa595ff4f8ab624ed816
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-16 15:22:50 +0700

    feat: add hyperlink for each image referenced to its own website

--- src/components/SkillsAndStrengths.jsx
+++ src/components/SkillsAndStrengths.jsx
@@ -1,19 +1,19 @@
 const techSkills = [
-  { title: "HTML5", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" },
-  { title: "CSS3", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" },
-  { title: "JavaScript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" },
-  { title: "TypeScript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" },
-  { title: "React", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" },
-  { title: "Svelte", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/svelte/svelte-original.svg" },
-  { title: "Tailwind CSS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" },
-  { title: "Node.js", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" },
-  { title: "Vite", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" },
-  { title: "MySQL", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" },
-  { title: "C++", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" },
-  { title: "Python", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" },
-  { title: "Git", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" },
-  { title: "Arduino", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/arduino/arduino-original.svg" },
-  { title: "Linux", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg" },
+  { title: "HTML5", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg", href: "https://html.spec.whatwg.org" },
+  { title: "CSS3", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg", href: "https://www.w3.org/Style/CSS" },
+  { title: "JavaScript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg", href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+  { title: "TypeScript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg", href: "https://www.typescriptlang.org" },
+  { title: "React", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg", href: "https://react.dev" },
+  { title: "Svelte", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/svelte/svelte-original.svg", href: "https://svelte.dev" },
+  { title: "Tailwind CSS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg", href: "https://tailwindcss.com" },
+  { title: "Node.js", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg", href: "https://nodejs.org" },
+  { title: "Vite", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg", href: "https://vitejs.dev" },
+  { title: "MySQL", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg", href: "https://www.mysql.com" },
+  { title: "C++", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg", href: "https://cplusplus.com" },
+  { title: "Python", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg", href: "https://www.python.org" },
+  { title: "Git", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg", href: "https://git-scm.com" },
+  { title: "Arduino", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/arduino/arduino-original.svg", href: "https://www.arduino.cc" },
+  { title: "Linux", src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg", href: "https://www.linux.org" },
 ];
 
 export default function SkillsAndStrengths() {
@@ -37,13 +37,15 @@ export default function SkillsAndStrengths() {
         <section className="w-full p-2 flex flex-wrap justify-center items-center gap-8">
           {techSkills.map((skill) => (
             <section data-aos="zoom-in" key={skill.title} className="max-w-40 bg-dark-blue border-b-2 border-b-neon-magenta border-l-2 border-l-neon-magenta border-r-2 border-r-neon-cyan border-t-2 border-t-neon-cyan p-6 rounded-[17px] space-y-6 hover:shadow-lg hover:shadow-neon-magenta">
+              <a href={skill.href} target="_blank" rel="noopener noreferrer">
                 <img
-                    className="w-full"
-                    title={skill.title}
-                    aria-label={skill.title}
-                    src={skill.src}
+                  className="w-full"
+                  title={skill.title}
+                  aria-label={skill.title}
+                  src={skill.src}
                 />
-                <div class="text-center w-full">{skill.title}</div>
+              </a>
+              <div className="text-center w-full">{skill.title}</div>
             </section>
           ))}
         </section>
